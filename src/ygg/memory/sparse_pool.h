3f3960a813dc130fb87935ff316281239e0cd594
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2022-02-04 14:34:57 +0100

    Created sparse_object_pool. Fixed CMakeLists.txt (added compile flags for Yggdrasil-Application)

---
+++ src/ygg/memory/sparse_pool.h
@@ -0,0 +1,88 @@
+// Copyright 2022 Robert Ryan. See Licence.md.
+
+#pragma once
+
+#include <vector>
+
+namespace ygg::memory
+{
+    /**
+     * @brief A non-iterable object store, to provide faster allocation objects stored on the heap.
+     * @tparam T The type that will be stored.
+    */
+    template<typename T>
+    class sparse_pool
+    {
+        static_assert(sizeof(T) >= sizeof(std::size_t),
+            "This implementation of sparse_pool stores the linked list "
+            "inside the data store itself. The index size is sizeof(std::size_t).");
+    public:
+        /**
+         * @brief Emplaces an element into this pool, returning the index.
+         * @details The order of insertion after removal is defined by a linked list.
+         * This function must be externally synchronized.
+         * @tparam ...Args The forward argument types of T's constructor.
+         * @param ...args The forward arguments of T's constructor.
+         * @return The inserted element.
+        */
+        template<class... Args>
+        std::size_t emplace(Args&&... args)
+        {
+            std::size_t idx = m_head;
+            if (idx != G_NO_HEAD) {
+                m_head = *reinterpret_cast<std::size_t*>(&m_vector[idx]);
+                m_vector[idx] = T(std::forward<Args>(args)...);
+                return idx;
+            } else {
+                m_vector.emplace_back(std::forward<Args>(args)...);
+                return m_vector.size() - 1ull;
+            }
+        }
+
+        /**
+         * @brief Removes an element from this pool.
+         * @details Removes an element and changes this pools head to idx.
+         * This function must be externally synchronized.
+         * @param idx The index of the element.
+        */
+        void remove(std::size_t idx)
+        {
+            m_vector[idx].~T();
+            *reinterpret_cast<std::size_t*> (&m_vector[idx]) = m_head;
+            m_head = idx;
+        }
+
+        /**
+         * @brief Returns the element associated with the provided index `idx`.
+         * @param idx The index of the element.
+         * @return The element associated with idx.
+        */
+        [[nodiscard]] T& at(std::size_t idx) { return m_vector.at(idx); }
+
+        /**
+         * @brief Returns the element associated with the provided index `idx`.
+         * @param idx The index of the element.
+         * @return The element associated with idx.
+        */
+        [[nodiscard]] const T& at(std::size_t idx) const { return m_vector.at(idx); }
+
+        /**
+         * @brief Returns the element associated with the provided index `idx`.
+         * @param idx The index of the element.
+         * @return The element associated with idx.
+        */
+        [[nodiscard]] T& operator[](std::size_t idx) { return m_vector[idx]; }
+
+        /**
+         * @brief Returns the element associated with the provided index `idx`.
+         * @param idx The index of the element.
+         * @return The element associated with idx.
+        */
+        [[nodiscard]] const T& operator[](std::size_t idx) const { return m_vector[idx]; }
+
+    private:
+        static constexpr inline uint64_t G_NO_HEAD = ~0ull;
+        std::vector<T> m_vector;
+        std::size_t m_head = G_NO_HEAD;
+    };
+}
--- src/ygg_application/main.cpp
+++ src/ygg_application/main.cpp
@@ -1,8 +1,23 @@
 // Copyright 2022 Robert Ryan. See Licence.md.
 
+#include "ygg/memory/sparse_pool.h"
+
 #include <cstdint>
+#include <iostream>
+#include <thread>
 
 int32_t main(uint32_t*, const char*[])
 {
+    ygg::memory::sparse_pool<uint64_t> pool;
+    pool.emplace(0);
+    pool.emplace(1);
+    pool.emplace(2);
+    pool.remove(0);
+    pool.remove(1);
+    pool.remove(2);
+    std::cout << pool.emplace(42) << std::endl;
+    std::cout << pool.emplace(420) << std::endl;
+    std::cout << pool.emplace(4200) << std::endl;
+
     return 0;
 }
