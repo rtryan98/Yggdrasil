3d1cb3a7587bddc0cb87d7603144307cc9c1958d
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2022-04-01 01:01:48 +0200

    Updated and created more image utils. Added related documentation.

--- src/ygg/vulkan/image_utils.cpp
+++ src/ygg/vulkan/image_utils.cpp
@@ -2,11 +2,13 @@
 
 #include "ygg/vulkan/image_utils.h"
 
+#include "ygg/vulkan/resource.h"
+
 #include <volk.h>
 
 namespace ygg::vk::img_utils
 {
-    VkImageSubresourceRange get_full_image_subresource_range(VkImageAspectFlags aspect_flags)
+    VkImageSubresourceRange get_image_subresource_range(VkImageAspectFlags aspect_flags)
     {
         VkImageSubresourceRange result = {
             .aspectMask = aspect_flags,
@@ -16,5 +18,56 @@ namespace ygg::vk::img_utils
             .layerCount = VK_REMAINING_ARRAY_LAYERS
         };
         return result;
+    }
+
+    VkImageSubresourceRange get_default_image_subresource_range(const Image_info& img_info)
+    {
+        return get_image_subresource_range(get_default_aspect_mask(img_info.format));
+    }
+
+    VkImageAspectFlags get_default_aspect_mask(VkFormat format)
+    {
+        switch (format) {
+        default:                            return VK_IMAGE_ASPECT_COLOR_BIT;
+        case VK_FORMAT_D16_UNORM:           ;
+        case VK_FORMAT_X8_D24_UNORM_PACK32: ;
+        case VK_FORMAT_D32_SFLOAT:          return VK_IMAGE_ASPECT_DEPTH_BIT;
+        case VK_FORMAT_S8_UINT:             return VK_IMAGE_ASPECT_STENCIL_BIT;
+        case VK_FORMAT_D16_UNORM_S8_UINT:   ;
+        case VK_FORMAT_D24_UNORM_S8_UINT:   ;
+        case VK_FORMAT_D32_SFLOAT_S8_UINT:  return VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+        case VK_FORMAT_UNDEFINED:           return 0;
+        }
+    }
+
+    VkImageViewType get_default_image_view_type(const Image_info& img_info)
+    {
+        switch (img_info.type)
+        {
+        default: ;
+        case VK_IMAGE_TYPE_1D:
+            if (img_info.array_layers > 1)
+                return VK_IMAGE_VIEW_TYPE_1D_ARRAY;
+            else
+                return VK_IMAGE_VIEW_TYPE_1D;
+        case VK_IMAGE_TYPE_2D:
+            if (img_info.array_layers > 1)
+                return VK_IMAGE_VIEW_TYPE_2D_ARRAY;
+            else
+                return VK_IMAGE_VIEW_TYPE_2D;
+        case VK_IMAGE_TYPE_3D:
+            return VK_IMAGE_VIEW_TYPE_3D;
+        }
+    }
+
+    VkImageCreateFlags get_default_image_create_flags(const Image_info& img_info)
+    {
+        VkImageCreateFlags flags = 0;
+        if ((img_info.array_layers % 6 == 0) && (img_info.type == VK_IMAGE_TYPE_2D))
+            flags |= VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT;
+        if (img_info.type == VK_IMAGE_TYPE_3D)
+            flags |= VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT;
+
+        return flags;
     }
 }
--- src/ygg/vulkan/image_utils.h
+++ src/ygg/vulkan/image_utils.h
@@ -4,7 +4,55 @@
 
 #include "ygg/vulkan/vk_forward_decl.h"
 
-namespace ygg::vk::img_utils
+namespace ygg::vk
 {
-    VkImageSubresourceRange get_full_image_subresource_range(VkImageAspectFlags aspect_flags);
+    struct Image_info;
+
+    namespace img_utils
+    {
+        /**
+         * @brief Creates a `VkImageSubresourceRange` for an entire image.
+         * @details Selects all mip levels and all array layers.
+         * @param aspect_flags Image aspect flags to use for the subresource range.
+         * @return A `VkImageAspectFlags` for the full image with the given `VkImageAspectFlags`.
+        */
+        VkImageSubresourceRange get_image_subresource_range(VkImageAspectFlags aspect_flags);
+
+        /**
+         * @brief Convenience function for speeding up creating a subresource range for an image.
+         * @details Equivalent to calling `get_image_subresource_range(get_default_aspect_mask(img_info.format))`.
+         * @param img_info The `Image_info` to derive the `VkImageSubresourceRange` from.
+         * @return A `VkImageSubresourceRange` for the full image derived from the given `Image_info`.
+        */
+        VkImageSubresourceRange get_default_image_subresource_range(const Image_info& img_info);
+
+        /**
+         * @brief Convenience function for speeding up selection of the aspect mask.
+         * @details Returns `VK_IMAGE_ASPECT_COLOR_BIT` for all formats except depth / stencil
+         * and undefined formats. Doesn't do any plane selection for multi-planar formats. Does
+         * not split up depth-stencil aspects in combined depth-stencil formats.
+         * @param format The format from which to derive the `VkImageAspectFlags` from.
+         * @return `VkImageAspectFlags` for the image derived from the given `VkFormat`.
+        */
+        VkImageAspectFlags get_default_aspect_mask(VkFormat format);
+
+        /**
+         * @brief Convenience function for speeding up selecting the image view type.
+         * @details Does not return a `VkImageView` of type `VK_IMAGE_VIEW_TYPE_CUBE`
+         * or `VK_IMAGE_VIEW_TYPE_CUBE_ARRAY`.
+         * @param img_info The `Image_info` to derive the `VkImageViewType` from.
+         * @return A `VkImageViewType` for the image derived from the given `Image_info`.
+        */
+        VkImageViewType get_default_image_view_type(const Image_info& img_info);
+
+        /**
+         * @brief Convenience function for speeding up selecting `VkImageCreateFlags`.
+         * @details Returns `VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT` for 2D-images that
+         * use multiples of 6 layers and `VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT` for
+         * 3D images.
+         * @param img_info The `Image_info` to derive the `VkImageCreateFlags` from.
+         * @return `VkImageCreateFlags` for the image derived from the given `Image_info`.
+        */
+        VkImageCreateFlags get_default_image_create_flags(const Image_info& img_info);
+    }
 }
